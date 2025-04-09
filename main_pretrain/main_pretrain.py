814696f5d5edb692e63aebaa07872a37440b2ad5
Niccolo <niccolo.mcconnell@gmail.com>
2025-04-09 13:17:31 +0100

    update

--- main_pretrain/main_pretrain.py
+++ main_pretrain/main_pretrain.py
@@ -72,7 +72,7 @@ def get_args_parser():
     parser.add_argument('--warmup_epochs', type=int, default=20, metavar='N',
                         help='epochs to warmup LR')
     # Dataset parameters
-    parser.add_argument('--data_path', default='data_path', type=str,
+    parser.add_argument('--data_path', default='./example_paths_pretrain.txt', type=str,
                         help='dataset path')
     parser.add_argument('--output_dir', default='output_dir', # fill this
                         help='path where to save, empty for no saving')
